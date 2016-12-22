e6cf09455bdabc6a018ab6cd5fca24e5b78c227a
ryenguyen7411 <ryenguyen7411@gmail.com>
2016-12-22 22:04:26 +0700

    - Add some button

--- project/main.py
+++ project/main.py
@@ -1,17 +1,31 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QPushButton
+from PyQt5.QtCore import QCoreApplication
 
+class main_window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setupGUI()
 
-class main_window(object):
-    def setupGUI(self, window):
-        window.resize(800, 600)
-        window.setWindowTitle("Laptop Consulting")
+    def setupGUI(self):
+        self.resize(800, 600)
+        self.setWindowTitle("Laptop Consulting")
+        self.center()
+
+        btn_exit = QPushButton('Quit', self)
+        btn_exit.clicked.connect(QCoreApplication.instance().quit)
+        btn_exit.resize(btn_exit.sizeHint())
+        btn_exit.move(50, 50)
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    w = QWidget()
     window = main_window()
-    window.setupGUI(w)
-    w.show()
+    window.show()
     sys.exit(app.exec_())
