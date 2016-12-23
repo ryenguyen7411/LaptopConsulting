b42e0ad8396eeeb2b85e29ba16d45f91d01376c4
ryenguyen7411 <ryenguyen7411@gmail.com>
2016-12-23 07:44:07 +0700

    - Add some UI

--- project/main.py
+++ project/main.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QPushButton
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
 
 class main_window(QWidget):
     def __init__(self):
@@ -12,16 +13,36 @@ class main_window(QWidget):
         self.setWindowTitle("Laptop Consulting")
         self.center()
 
-        btn_exit = QPushButton('Quit', self)
-        btn_exit.clicked.connect(QCoreApplication.instance().quit)
-        btn_exit.resize(btn_exit.sizeHint())
-        btn_exit.move(50, 50)
+        # btn_exit = QPushButton('Quit', self)
+        # btn_exit.clicked.connect(QCoreApplication.instance().quit)
+        # btn_exit.resize(btn_exit.sizeHint())
+        # btn_exit.move(50, 50)
+
+        btn_mode_laptop = QRadioButton('Laptop', self)
+        btn_mode_laptop.setChecked(True)
+        btn_mode_laptop.toggled.connect(lambda:self.toggle_mode(btn_mode_laptop))
+        btn_mode_laptop.move(50, 20)
+
+        btn_mode_desktop = QRadioButton('Build PC', self)
+        btn_mode_desktop.toggled.connect(lambda:self.toggle_mode(btn_mode_desktop))
+        btn_mode_desktop.move(150, 20)
+
+
+        lbl_ram = QLabel('RAM', self)
+        lbl_ram.move(50, 100)
 
     def center(self):
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+
+    def toggle_mode(self, button):
+        a = 0
+        # if button.text() == 'Laptop':
+        #     abc
+        # elif button.text() == 'Build PC':
+
 
 
 if __name__ == "__main__":
