36bc9f1ee81b6ef6f92a762008f33d7474823299
Nguyen Minh Phuc <comfyneet@gmail.com>
2017-01-14 11:34:47 +0700

    Change detail form design

--- laptop_consulting/laptop_consulting/DetailForm.Designer.cs
+++ laptop_consulting/laptop_consulting/DetailForm.Designer.cs
@@ -40,10 +40,11 @@
             this.lbl_detail_price.AutoSize = true;
             this.lbl_detail_price.Depth = 0;
             this.lbl_detail_price.Font = new System.Drawing.Font("Roboto", 11F);
-            this.lbl_detail_price.Location = new System.Drawing.Point(611, 136);
+            this.lbl_detail_price.Location = new System.Drawing.Point(407, 88);
+            this.lbl_detail_price.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_detail_price.MouseState = MaterialSkin.MouseState.HOVER;
             this.lbl_detail_price.Name = "lbl_detail_price";
-            this.lbl_detail_price.Size = new System.Drawing.Size(62, 27);
+            this.lbl_detail_price.Size = new System.Drawing.Size(43, 19);
             this.lbl_detail_price.TabIndex = 0;
             this.lbl_detail_price.Text = "Price";
             // 
@@ -57,12 +58,13 @@
             this.listview_detail.Font = new System.Drawing.Font("Roboto", 18F);
             this.listview_detail.FullRowSelect = true;
             this.listview_detail.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
-            this.listview_detail.Location = new System.Drawing.Point(526, 203);
+            this.listview_detail.Location = new System.Drawing.Point(136, 132);
+            this.listview_detail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listview_detail.MouseLocation = new System.Drawing.Point(-1, -1);
             this.listview_detail.MouseState = MaterialSkin.MouseState.OUT;
             this.listview_detail.Name = "listview_detail";
             this.listview_detail.OwnerDraw = true;
-            this.listview_detail.Size = new System.Drawing.Size(433, 311);
+            this.listview_detail.Size = new System.Drawing.Size(504, 202);
             this.listview_detail.TabIndex = 1;
             this.listview_detail.UseCompatibleStateImageBehavior = false;
             this.listview_detail.View = System.Windows.Forms.View.Details;
@@ -80,11 +82,12 @@
             // materialRaisedButton1
             // 
             this.materialRaisedButton1.Depth = 0;
-            this.materialRaisedButton1.Location = new System.Drawing.Point(12, 548);
+            this.materialRaisedButton1.Location = new System.Drawing.Point(8, 356);
+            this.materialRaisedButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialRaisedButton1.Name = "materialRaisedButton1";
             this.materialRaisedButton1.Primary = true;
-            this.materialRaisedButton1.Size = new System.Drawing.Size(75, 40);
+            this.materialRaisedButton1.Size = new System.Drawing.Size(50, 26);
             this.materialRaisedButton1.TabIndex = 2;
             this.materialRaisedButton1.Text = "Back";
             this.materialRaisedButton1.UseVisualStyleBackColor = true;
@@ -92,12 +95,13 @@
             // 
             // DetailForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1024, 600);
+            this.ClientSize = new System.Drawing.Size(683, 390);
             this.Controls.Add(this.materialRaisedButton1);
             this.Controls.Add(this.listview_detail);
             this.Controls.Add(this.lbl_detail_price);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "DetailForm";
             this.Text = "DetailForm";
             this.ResumeLayout(false);
